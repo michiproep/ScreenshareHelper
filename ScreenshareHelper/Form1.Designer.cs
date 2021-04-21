53cbb9c494685db4a8627e61deab01b5e3699e8e
Michael Pröpster <imp@outlook.de>
2021-04-21 20:12:20 +0200

    Projektdateien hinzufügen.

---
+++ ScreenshareHelper/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace ScreenshareHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonSetCaptureArea = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            // 
+            // button2
+            // 
+            this.buttonSetCaptureArea.Location = new System.Drawing.Point(12, 12);
+            this.buttonSetCaptureArea.Name = "button2";
+            this.buttonSetCaptureArea.Size = new System.Drawing.Size(94, 29);
+            this.buttonSetCaptureArea.TabIndex = 1;
+            this.buttonSetCaptureArea.Text = "Set";
+            this.buttonSetCaptureArea.UseVisualStyleBackColor = true;
+            this.buttonSetCaptureArea.Click += new System.EventHandler(this.buttonSetCaptureArea_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSetCaptureArea);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "ScreensharingHelper";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonSetCaptureArea;
+    }
+}
+
---
+++ ScreenshareHelper/Form1.cs
@@ -0,0 +1,150 @@
+﻿using ScreenshareHelper.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenshareHelper
+{
+    public partial class Form1 : Form
+    {
+        readonly Color transKey = Color.SaddleBrown;
+        private bool isActive = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.TransparencyKey = transKey;
+            RestoreWindowPosition();
+        }
+        
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            if (isActive)
+                e.Graphics.Clear(transKey);
+            else
+                paint(e.Graphics);
+        }
+
+        #region Cursor
+        [StructLayout(LayoutKind.Sequential)]
+        struct CURSORINFO
+        {
+            public Int32 cbSize;
+            public Int32 flags;
+            public IntPtr hCursor;
+            public POINTAPI ptScreenPos;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct POINTAPI
+        {
+            public int x;
+            public int y;
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorInfo(out CURSORINFO pci);
+
+        [DllImport("user32.dll")]
+        static extern bool DrawIcon(IntPtr hDC, int X, int Y, IntPtr hIcon);
+
+        const Int32 CURSOR_SHOWING = 0x00000001;
+        #endregion Cursor
+        private void paint(Graphics graphics, bool withMouse = true)
+        {
+            graphics.CopyFromScreen(Settings.Default.CaptureLocation.X, Settings.Default.CaptureLocation.Y, 0, 0, Settings.Default.CaptureSize);
+            if (withMouse)
+            {
+                CURSORINFO pci;
+                pci.cbSize = System.Runtime.InteropServices.Marshal.SizeOf(typeof(CURSORINFO));
+
+                if (GetCursorInfo(out pci))
+                {
+                    if (pci.flags == CURSOR_SHOWING)
+                    {
+                        const int offset = 5;
+                        DrawIcon(graphics.GetHdc(), 
+                            pci.ptScreenPos.x - Settings.Default.CaptureLocation.X - offset, 
+                            pci.ptScreenPos.y - Settings.Default.CaptureLocation.Y - offset, 
+                            pci.hCursor);
+                        graphics.ReleaseHdc();
+                    }
+                }
+            }
+        }
+      
+        private void buttonSetCaptureArea_Click(object sender, EventArgs e)
+        {
+            Settings.Default.CaptureLocation = this.Location;
+            Settings.Default.CaptureSize = this.Size;
+        }
+        
+        private void RestoreWindowPosition()
+        {
+            if (Settings.Default.HasSetDefaults)
+            {
+                this.Location = Settings.Default.Location;
+                this.Size = Settings.Default.Size;
+            }
+        }
+        private void SaveWindowPosition()
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                Settings.Default.Location = this.Location;
+                Settings.Default.Size = this.Size;
+            }
+            else
+            {
+                Settings.Default.Location = this.RestoreBounds.Location;
+                Settings.Default.Size = this.RestoreBounds.Size;
+            }
+
+            Settings.Default.HasSetDefaults = true;
+            Settings.Default.Save();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            isActive = true;
+            FormBorderStyle = FormBorderStyle.Sizable;
+            buttonSetCaptureArea.Visible = isActive;
+        }
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            this.Size = Settings.Default.CaptureSize;
+            isActive = false;
+            buttonSetCaptureArea.Visible = isActive;
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveWindowPosition();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread t = new Thread(() =>
+            {
+                while (true)
+                {
+                    if (!isActive)
+                        Invalidate();
+                    Thread.Sleep(10);
+                }
+            });
+            t.IsBackground = true;
+            t.Start();
+        }
+    }
+}
